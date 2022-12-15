633098fcc2107ff780b1c0b97c6558ba04ca79ed
JoshuaJeconia <117901765+JoshuaJeconia@users.noreply.github.com>
2022-12-15 16:17:38 +0700

    Add files via upload

---
+++ UG13_D_71220860 (2).py
@@ -0,0 +1,36 @@
+import turtle as t
+t.pensize(40)
+t.color("blue")
+
+#huruf J
+t.forward(100)
+t.backward(200)
+t.forward(100)
+t.right(90)
+t.forward(120)
+t.circle(-60,200)
+
+#giving space
+#angka 8
+#angka 6
+
+#angka 0
+t.pensize(40)
+t.color("blue")
+for i in range(2):
+ t.penup()
+ t.pendown()
+ t.circle(50)
+ #huruf P
+t.pensize(40)
+t.color("blue")
+t.circle(-60,200)
+t.left(90)
+t.forward(250)
+ 
+ #angka 6
+t.pensize(40)
+t.color("blue")
+t.circle(80,180)
+t.left(90)
+t.forward(250)
